c98c008dbcec22441863a03aa97e12c0b9ba8e05
Wechiii <matwechner@tsn.at>
2024-03-07 08:06:37 +0100

    .

---
+++ Aufgabe03_SchereSteinPapier/Classes/flaskServer.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import sqlite3
+
+app = Flask(__name__)
+
+
+def setup_db():
+    connection = sqlite3.connect('SchereSteinPapierEchseSpock.db')
+    cursor = connection.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS counts (id INTEGER PRIMARY KEY AUTOINCREMENT, symbole TEXT, count TEXT)")
+
+
+def speichern(symbole):
+    connection = sqlite3.connect('SchereSteinPapier.db')
+    cursor = connection.cursor()
+
+    cursor.execute("INSERT INTO counts (symbole, count) VALUES ('{}','{}').format(symbole['symbol'], symbole['count'])")
+
+    connection.commit()
+
+
+@app.route('/saveStats', methods=['POST'])
+def post_statistics():
+    if request.is_json:
+        speichern(request.json)
+        return 'Daten eingelesen'
+    else:
+        return 'Speicherfehler'
+
+
+if __name__ == '__main__':
+    setup_db()
+    app.run(debug=True)
